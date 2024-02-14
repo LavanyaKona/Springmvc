bbcc396cb76133dd8cc76316f563972b5d740a51
Lavanya <1999lavanyakona@gmail.com>
2024-02-14 14:36:30 -0600

    BMI calculator

---
+++ src/main/java/com/java/cis/BMIController.java
@@ -0,0 +1,28 @@
+package com.java.cis;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BMIController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String input(Locale locale, Model model) {
+        System.out.println("Home Page Requested, locale = " + locale);
+        return "inputForm";
+    }
+    @RequestMapping(value = "/bmi", method = RequestMethod.POST)
+    public String result(@RequestParam("height") double heightInInches,
+                         @RequestParam("weight") double weightInPounds,
+                         Model model) {
+        double bmi = weightInPounds / (heightInInches * heightInInches) * 703;
+        model.addAttribute("bmi", bmi);
+        return "bmiResult";
+    }
+}
