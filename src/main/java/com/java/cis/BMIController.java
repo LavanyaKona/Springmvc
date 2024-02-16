f37d9923519eb7ce4307c09858ee3a90ba9c19d4
Lavanya <1999lavanyakona@gmail.com>
2024-02-16 12:25:22 -0600

    Rest Controller - BMI REST controller

--- src/main/java/com/java/cis/BMIController.java
+++ src/main/java/com/java/cis/BMIController.java
@@ -1,14 +1,13 @@
 package com.java.cis;
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.util.Date;
-import java.util.Locale;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
 
 @Controller
 public class BMIController {
